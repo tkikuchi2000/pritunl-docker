0891fe20be3c21f53b7408b1faf4fe3f3af1e8f7
Tetsuya Kikuchi <tkikuchi2000@gmail.com>
2018-05-05 20:54:33 +0900

    Shutdown mongodb gracefully

---
+++ data/bin/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+trap_sigterm() {
+  echo '============ SIGTERM RECEIVED ============='
+  mongod --shutdown && rm -rf /data/db/mongod.lock
+  echo '============ MONGODB SHUTDOWN DONE ============='
+  sleep 3
+  exit 0
+}
+
+trap 'trap_sigterm' SIGTERM
+nohup mongod &
+
+while true; do
+  sleep 5
+done
+
+#vim: set nu sts=2 et si=2:
